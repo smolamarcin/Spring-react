a3da040d107f6669794218f208a5614c298e9671
Marcin Smola <marcin.smola7@gmail.com>
2020-10-31 19:06:47 +0100

    Work in progress...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App () {
-    const [message, setMessage] = useState("");
+class TodoItem extends Component {
+    static defaultProps = {
+        done: false
+    }
+    state = {
+        done: this.props.done,
+    }
 
-    useEffect(() => {
-        fetch('/api/employees/1')
-            .then(res => res)
-            .then(response => response.json())
-            .then(message => {
-            setMessage(message.firstName);
-            });
-    },[])
-    return (
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{message}</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    )
+    toggleDone = () => {
+        this.setState({ done: !this.state.done })
+    }
+    render() {
+        // const { text } = this.props.t;
+        return (
+            <div onClick={this.toggleDone} className={this.state.done ? 'doneTodo' : ''}>{this.props.text}</div>
+        );
+    }
+}
+class TodoList extends Component {
+    static defaultProps = {
+        tasks: [
+            { done: true, text: 'first task' },
+            { done: false, text: 'second task' }
+        ]
+    }
+    state = {
+        tasks: this.props.tasks,
+        draft: ''
+    }
+
+    addTask = () => {
+        // const {tasks, draft} = this.state
+        // const list = tasks
+        // list.push({text: draft})
+        // this.setState({tasks: list, draft: ''})
+        this.setState(prevState => (
+            {
+                tasks: [...prevState.tasks,
+                { text: this.state.draft }],
+                draft: ""
+            })
+        )
+    }
+    updateDraft = (event) => this.setState({ draft: event.target.value })
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                {this.state.tasks.map(task => <TodoItem text={task.text} done={task.done}></TodoItem>)}
+                <input type="text" onChange={this.updateDraft} value={this.state.draft}></input>
+                <button onClick={this.addTask}>Add</button>
+            </div>
+
+        );
+    }
 }
 
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <TodoList />
+            </div>
+        );
+    }
+};
 export default App;
